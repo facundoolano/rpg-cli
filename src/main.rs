83de24fd468a7c43fa30cca9290dad2fa2479604
Facundo Olano <facundo.olano@gmail.com>
2021-04-23 20:02:57 -0300

    player struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use player::Player;
+
+mod player;
+
 fn main() {
-    println!("Hello, world!");
+    let player = Player::new();
+    player_status(&player);
+}
+
+fn player_status(player: &Player) {
+    println!("{} lv {}", player.name, player.level);
+    println!("hp {}/{}", player.current_hp, player.max_hp);
+    println!("xp {} next level: TODO", player.xp);
 }
---
+++ src/player.rs
@@ -0,0 +1,28 @@
+pub struct Player {
+    pub name: String,
+
+    pub level: i32,
+    pub xp: i32,
+
+    pub max_hp: i32,
+    pub current_hp: i32,
+
+    pub strength: i32,
+    pub speed: i32,
+    pub luck: i32,
+}
+
+impl Player {
+    pub fn new() -> Player {
+        Self {
+            name: String::from("Hero"),
+            level: 1,
+            xp: 0,
+            max_hp: 20,
+            current_hp: 20,
+            strength: 10,
+            speed: 5,
+            luck: 3,
+        }
+    }
+}
