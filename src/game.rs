75a6434dbeac2538c00c4c2303b66f75b225eab9
Facundo Olano <facundo.olano@gmail.com>
2021-04-24 00:45:22 -0300

    load from datafile

--- src/game.rs
+++ src/game.rs
@@ -1,19 +1,34 @@
 use crate::player::Player;
 use serde::{Deserialize, Serialize};
-use std::path;
+use std::{fs, io};
+
+const DATA_FILE: &str = "~/.rpg/game";
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Game<'a> {
+pub struct Game {
     pub player: Player,
-
-    #[serde(borrow)]
-    pub location: &'a path::Path,
+    pub location: String,
 }
 
-impl Game<'_> {
+impl Game {
+    pub fn load() -> Result<Self, io::Error> {
+        match fs::read(DATA_FILE) {
+            Ok(data) => {
+                let game: Game = bincode::deserialize(&data).unwrap();
+                Ok(game)
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::NotFound => Ok(Self::new()),
+            Err(error) => Err(error),
+        }
+    }
+
+    pub fn save(&self) {
+        // TODO implement
+    }
+
     pub fn new() -> Self {
         Self {
-            location: path::Path::new("~"),
+            location: String::from("~"),
             player: Player::new(),
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ mod game;
 mod player;
 
 fn main() {
-    let game = Game::new();
+    let game = Game::load().unwrap();
+    game.save();
     player_status(&game);
 }
 
 fn player_status(game: &Game) {
     let player = &game.player;
-    println!("{}@{} ", player.name, game.location.display());
+    println!("{}@{} ", player.name, game.location);
     println!(
         "lv:{} hp:{}/{} xp:{}",
         player.level, player.current_hp, player.max_hp, player.xp
