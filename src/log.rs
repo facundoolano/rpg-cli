a21c9751f957c8d0b50a561eb1800bde3d51d1f0
Facundo Olano <facundo.olano@gmail.com>
2021-04-29 19:40:54 -0300

    consistent print function

--- src/log.rs
+++ src/log.rs
@@ -3,15 +3,15 @@ use crate::location::Location;
 use colored::*;
 
 pub fn enemy_appears(enemy: &Character, location: &Location) {
-    println!("{}", character_at(&enemy, &location));
+    log(&enemy, &location, "");
 }
 
 pub fn heal(player: &Character, location: &Location, recovered: i32) {
     if recovered > 0 {
-        println!(
-            "{} {}",
-            character_at(&player, &location),
-            format!("+{}hp", recovered).green()
+        log(
+            &player,
+            &location,
+            &format!("+{}hp", recovered).green().to_string(),
         );
     }
 }
@@ -23,15 +23,11 @@ pub fn attack(enemy: &Character, location: &Location, damage: i32) {
         format!("{}hp", -damage).white()
     };
 
-    println!(
-        "{} {}",
-        character_at(&enemy, &location),
-        damage
-    );
+    log(&enemy, &location, &damage.to_string());
 }
 
 pub fn battle_lost(player: &Character, location: &Location) {
-    println!("{} \u{1F480}", character_at(&player, &location));
+    log(&player, &location, "\u{1F480}");
 }
 
 pub fn battle_won(player: &Character, location: &Location, xp: i32, level_up: bool, gold: i32) {
@@ -41,45 +37,36 @@ pub fn battle_won(player: &Character, location: &Location, xp: i32, level_up: bo
         "".to_string()
     };
 
-    println!(
-        "{} {}{} {}",
-        character_at(&player, &location),
-        format!("+{}xp", xp).bold(),
-        level_str,
-        format!("+{}g", gold).yellow(),
+    log(
+        &player,
+        &location,
+        &format!(
+            "{}{} {}",
+            format!("+{}xp", xp).bold(),
+            level_str,
+            format!("+{}g", gold).yellow()
+        ),
     );
 }
 
 pub fn status(player: &Character, location: &Location) {
-    println!("{}", character_at(&player, &location));
-}
-
-impl std::fmt::Display for Character {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let name = if is_hero(&self) {
-            // FIXME use correct padding
-            " hero".bold().to_string()
-        } else {
-            self.name.yellow().bold().to_string()
-        };
-
-        write!(f, "{}[{}]", name, self.level)
-    }
+    log(&player, &location, "");
 }
 
 // HELPERS
 
-// NOTE: the pervasiveness of this function suggests that location should
-// be an attribute of character, and that this function should be the
-// implementation of the display trait
-fn character_at(character: &Character, location: &Location) -> String {
-    format!(
-        "    {}{}{}@{}",
-        character,
+/// Generic log function. At the moment all output of the game is structured as
+/// of a player status at some location, with an optional event suffix.
+fn log(character: &Character, location: &Location, suffix: &str) {
+    println!(
+        "    {}[{}]{}{}@{} {}",
+        name(&character),
+        character.level,
         hp_display(&character),
         xp_display(&character),
-        location
-    )
+        location,
+        suffix
+    );
 }
 
 fn hp_display(character: &Character) -> String {
@@ -108,6 +95,15 @@ fn bar_display(current: i32, total: i32, current_color: &str, missing_color: &st
         .collect::<String>()
         .color(missing_color);
     format!("[{}{}]", current, missing)
+}
+
+fn name(character: &Character) -> String {
+    if is_hero(&character) {
+        // FIXME use correct padding
+        " hero".bold().to_string()
+    } else {
+        character.name.yellow().bold().to_string()
+    }
 }
 
 fn is_hero(character: &Character) -> bool {
