79a204d564484582c82645845fd5727ee5fa50f3
Facundo Olano <facundo.olano@gmail.com>
2021-05-09 20:05:47 -0300

    generic equipment instead of sword

--- src/equipment.rs
+++ src/equipment.rs
@@ -2,20 +2,19 @@ use crate::character::class as character;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Sword {
+pub struct Equipment {
     level: i32,
 }
 
 
-impl Sword {
+impl Equipment {
     pub fn new(level: i32) -> Self {
         Self { level }
     }
 
     /// How many strength points get added to the player when
-    /// the weapon is equipped.
+    /// the item is equipped.
     pub fn strength(&self) -> i32 {
-
         // get the base strength of the hero at this level
         // TODO make this a character method?
         let class = &character::Class::HERO;
@@ -23,6 +22,6 @@ impl Sword {
         let player_strength = class.start_strength as f64 * inc_rate.powi(self.level);
 
         // calculate the added strength as a function of the player strength
-        (player_strength * 1.5) as i32
+        (player_strength * 1.5).round() as i32
     }
 }
