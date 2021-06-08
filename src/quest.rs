91881b70dfc8d72c6395d0e14c41da3d19e65b6e
Facundo Olano <facundo.olano@gmail.com>
2021-06-07 21:11:42 -0300

    setup basic quest

--- src/quest.rs
+++ src/quest.rs
@@ -1,11 +1,12 @@
 use core::fmt;
 use crate::game;
 use crate::character::Character;
+use serde::{Deserialize, Serialize};
 
 #[typetag::serde(tag = "type")]
-pub trait Quest: fmt::Display {
+pub trait Quest {
     /// What to show in the TODO quests list
-    fn description (&self) -> String;
+    fn description (&self) -> &str;
 
     /// Whether this quest should appear in the quest list
     fn is_visible(&self, _game: &game::Game) -> bool {
@@ -26,6 +27,18 @@ pub trait Quest: fmt::Display {
     fn tombstone(&mut self) {}
 }
 
+impl fmt::Display for dyn Quest {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.description())
+    }
+}
+
+/// TODO
+pub fn setup (game: &mut game::Game) {
+    game.quests.push(Box::new(WinBattle{done:false}));
+}
+
+/// TODO
 pub fn list() {
     todo!();
 }
@@ -36,27 +49,27 @@ pub fn list() {
 // alias for quests passed dynamically
 type QuestRef<'a> = &'a mut Box<dyn Quest>;
 
-pub fn battle_won(game: &mut game::Game, enemy: &Character, levels_up: i32) {
+pub fn handle_battle_won(game: &mut game::Game, enemy: &Character, levels_up: i32) {
     let handler = |q: QuestRef| q.battle_won(&enemy, levels_up);
-    update(game, &handler);
+    handle(game, &handler);
 }
 
-pub fn item_bought(game: &mut game::Game, name: &str) {
+pub fn handle_item_bought(game: &mut game::Game, name: &str) {
     let handler = |q: QuestRef| q.item_bought(name);
-    update(game, &handler);
+    handle(game, &handler);
 }
 
-pub fn item_used(game: &mut game::Game, name: &str) {
+pub fn handle_item_used(game: &mut game::Game, name: &str) {
     let handler = |q: QuestRef| q.item_used(name);
-    update(game, &handler);
+    handle(game, &handler);
 }
 
-pub fn tombstone(game: &mut game::Game) {
+pub fn handle_tombstone(game: &mut game::Game) {
     let handler = |q: QuestRef| q.tombstone();
-    update(game, &handler);
+    handle(game, &handler);
 }
 
-fn update(game: &mut game::Game, handler: &dyn Fn(QuestRef)) {
+fn handle(game: &mut game::Game, handler: &dyn Fn(QuestRef)) {
     for quest in game.quests.iter_mut() {
         handler(quest);
         if quest.is_done() {
@@ -64,5 +77,32 @@ fn update(game: &mut game::Game, handler: &dyn Fn(QuestRef)) {
             game.gold += reward;
             // TODO log::quest_done(reward);
         }
+    }
+}
+
+// QUEST DEFINITIONS
+// TODO consider moving these to other files
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct WinBattle {
+    done: bool
+}
+
+#[typetag::serde]
+impl Quest for WinBattle {
+    fn description (&self) -> &str {
+        "Win a battle"
+    }
+
+    fn is_done(&self) -> bool {
+        self.done
+    }
+
+    fn reward(&self) -> i32 {
+        100
+    }
+
+    fn battle_won(&mut self, _enemy: &Character, _levels_up: i32) {
+        self.done = true;
     }
 }
