ddc9f0938f4795bbaa907e5636a5942215f155a7
Haowei Gao <haowei.gao@nextroll.com>
2021-06-11 09:45:37 -0700

    semicolon

--- src/main.rs
+++ src/main.rs
@@ -238,13 +238,13 @@ fn perform_ascension(class: &Option<String>, game: &mut Game) {
                 "warrior"   => {
                     game.player = Character::ascend(&Class::WARRIOR, game.player.level,
                                                     game.player.sword.take(),
-                                                    game.player.shield.take())
+                                                    game.player.shield.take());
                     println!("You have ascended to the Warrior class")
                 },
                 "rogue"     => {
                     game.player = Character::ascend(&Class::ROGUE, game.player.level,
                                                     game.player.sword.take(),
-                                                    game.player.shield.take())
+                                                    game.player.shield.take());
                     println!("You have ascended to the Rogue class")
                 },
                 _ => println!("Please select a valid class")
