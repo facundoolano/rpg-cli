7ecb5d88c70d55e53ac8b615f6329bbc2a58dd6d
Facundo Olano <facundo.olano@gmail.com>
2021-08-14 18:46:44 -0300

    use enums instead

--- src/item/ring.rs
+++ src/item/ring.rs
@@ -4,9 +4,14 @@ use serde::{Deserialize, Serialize};
 
 /// Rings are a kind of equipment that produce arbitrary effects hooked in
 /// different places of the game, e.g. increase a stat, double gold gained, etc.
-#[typetag::serde(tag = "type")]
-pub trait Ring {
-    fn key(&self) -> String;
+#[derive(Serialize, Deserialize)]
+pub enum Ring {
+    Void,
+    AttackRing,
+    DeffenseRing,
+    MagicRing,
+    MPRing,
+    HPRing,
 }
 
 /// The character is allowed to hold two rings.
@@ -14,8 +19,8 @@ pub trait Ring {
 /// handling the equipping and calculating the net combined effect of the two rings.
 #[derive(Serialize, Deserialize, Default)]
 pub struct RingPair {
-    left: Option<Box<dyn Ring>>,
-    right: Option<Box<dyn Ring>>,
+    left: Option<Ring>,
+    right: Option<Ring>,
 }
 
 impl RingPair {
@@ -28,11 +33,37 @@ impl RingPair {
 
     /// Put the given ring in the left, moving the left (if any) to the right
     /// and returning the right (if any)
-    fn equip(&mut self, ring: Box<dyn Ring>) -> Option<Box<dyn Ring>> {
+    fn equip(&mut self, ring: Ring) -> Option<Ring> {
         if let Some(old_left) = self.left.replace(ring) {
             return self.right.replace(old_left);
         }
         None
+    }
+
+    fn apply_mult(&self, base: i32, fun: fn(&Ring) -> f64) -> i32 {
+        let base = base as f64;
+        let mult = self.left.as_ref().map_or(1.0, fun) + self.right.as_ref().map_or(1.0, fun);
+        (base * mult).round() as i32
+    }
+
+    pub fn attack(&self, base: i32) -> i32 {
+        self.apply_mult(base, Ring::attack_mult)
+    }
+
+    pub fn deffense(&self, base: i32) -> i32 {
+        self.apply_mult(base, Ring::deffense_mult)
+    }
+
+    pub fn magic(&self, base: i32) -> i32 {
+        self.apply_mult(base, Ring::magic_mult)
+    }
+
+    pub fn mp(&self, base: i32) -> i32 {
+        self.apply_mult(base, Ring::mp_mult)
+    }
+
+    pub fn hp(&self, base: i32) -> i32 {
+        self.apply_mult(base, Ring::hp_mult)
     }
 }
 
@@ -40,7 +71,7 @@ impl RingPair {
 /// used them to equip them.
 #[derive(Serialize, Deserialize)]
 pub struct RingItem {
-    ring: Box<dyn Ring>,
+    ring: Ring,
 }
 
 #[typetag::serde]
@@ -51,7 +82,7 @@ impl Item for RingItem {
     fn apply(&mut self, game: &mut game::Game) {
         // In order to move out the inner ring (without having to change it to an Option)
         // replace its memory with a throw away Void ring
-        let ring = std::mem::replace(&mut self.ring, Box::new(Void));
+        let ring = std::mem::replace(&mut self.ring, Ring::Void);
 
         if let Some(removed) = game.player.rings.equip(ring) {
             let key = removed.key();
@@ -60,14 +91,39 @@ impl Item for RingItem {
     }
 }
 
+impl Ring {
+    fn key(&self) -> &'static str {
+        match self {
+            Ring::Void => "void",
+            Ring::AttackRing => "attack",
+            Ring::DeffenseRing => "deffense",
+            Ring::MagicRing => "magic",
+            Ring::MPRing => "mp",
+            Ring::HPRing => "hp",
+        }
+    }
 
+    fn attack_mult(&self) -> f64 {
+        if let Ring::AttackRing = self {
+            1.5
+        } else {
+            1.0
+        }
+    }
 
-#[derive(Serialize, Deserialize)]
-pub struct Void;
+    fn deffense_mult(&self) -> f64 {
+        1.0
+    }
 
-#[typetag::serde]
-impl Ring for Void {
-    fn key(&self) -> String {
-        String::from("void")
+    fn magic_mult(&self) -> f64 {
+        1.0
+    }
+
+    fn mp_mult(&self) -> f64 {
+        1.0
+    }
+
+    fn hp_mult(&self) -> f64 {
+        1.0
     }
 }
