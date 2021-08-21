70bfc15a2442bf934deb6f8e896d34bcf66ecbd9
Facundo Olano <facundo.olano@gmail.com>
2021-08-21 18:16:42 -0300

    add a test

--- src/quest/mod.rs
+++ src/quest/mod.rs
@@ -177,6 +177,8 @@ impl fmt::Display for dyn Quest {
 mod tests {
     use super::*;
     use crate::character::Character;
+    use crate::item;
+    use crate::item::Item;
 
     #[test]
     fn test_quest_status() {
@@ -275,6 +277,33 @@ mod tests {
             count_status(&game.quests, Status::Unlocked)
         );
         assert_eq!(1, count_status(&game.quests, Status::Completed));
+    }
+
+    #[test]
+    fn test_level_up() {
+        let mut game = game::Game::new();
+        game.quests.quests = vec![
+            (Status::Unlocked, 10, Box::new(level::ReachLevel::new(2))),
+            (Status::Unlocked, 10, Box::new(level::ReachLevel::new(3))),
+        ];
+
+        game.player.level = 2;
+        event::Event::emit(
+            &mut game,
+            event::Event::LevelUp {
+                count: 1,
+                class: "warrior".to_string(),
+                current: 2,
+            },
+        );
+
+        assert_eq!(Status::Completed, game.quests.quests[0].0);
+        assert_eq!(Status::Unlocked, game.quests.quests[1].0);
+
+        let stone = item::stone::Level;
+        stone.apply(&mut game);
+        assert_eq!(Status::Completed, game.quests.quests[0].0);
+        assert_eq!(Status::Completed, game.quests.quests[1].0);
     }
 
     fn count_status(quests: &QuestList, status: Status) -> usize {
